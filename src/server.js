f55dcb092f43515c7e11f017690d057faaeaab2a
Jaaron <jsw6194@rit.edu>
2024-02-18 18:24:53 -0500

    reusing some old code

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const url = require('url');
+
+const urlStruct = {
+
+};
+
+const onRequest = (request, response) => {
+    const parsedUrl = url.parse(request.url);
+
+    
+};
